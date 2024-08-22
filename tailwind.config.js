77b307d085c3d00fa88f21ba3c2b0a15d57a1b38
sm-mehedi <hasanmehedi63395@gmail.com>
2024-08-22 16:29:39 +0600

    navbar and footer with responsive done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+ neon: '#ABEF5F',
+ blackcustom:'#1C1C1C',
+ wri:'#737373'
+      }
+    },
+  },
+  plugins: [],
+}
